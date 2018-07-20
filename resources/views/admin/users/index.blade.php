79ea66d85738cf2357e345f530a28fae7dd30275
Anderson <anderson@sinprosp.org.br>
2018-07-20 10:11:09 -0300

    Initial commit

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h3>Listagem de Usuários</h3>
+            {!! Button::primary('Novo usuário')->asLinkTo(route('admin.users.create')) !!}
+        </div>
+
+        <div class="row">
+            {!! Table::withContents($users->items())
+                    ->striped()
+                    ->callback('Ações', function($field, $model){
+                         $linkEdit = route('admin.users.edit', ['user' => $model->id]);
+                         $linkShow = route('admin.users.show', ['user' => $model->id]);
+                         return Button::link('Editar')->asLinkTo($linkEdit) . ' | ' . Button::link('Ver')->asLinkTo($linkShow);
+                    })
+             !!}
+        </div>
+
+        {{--paginação--}}
+        {!! $users->links() !!}
+    </div>
+@endsection()
